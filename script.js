f81d2c3f1ec5fdde2fa0d39d4c9249e36bfdabd5
Glitch (glitch-hello-website) <none>
2021-10-16 06:02:51 +0000

    🐙👔 Checkpoint ./script.js:32302306/82 ./index.html:32302306/39

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ let lat, lng;
 const scrypt = document.createElement("script");
 scrypt.src = `https://maps.googleapis.com/maps/api/js?key=${constants.API_KEY}&callback=initMap`; //&v=weekly&channel=2
 scrypt.async = true;
-const header1 = document.querySelector('h1');
-
-header1.textContent = "Hola";
-
 
 function getCurrentPosition() {
   return new Promise((resolve, reject) => {
@@ -23,7 +19,7 @@ window.initMap = async function() {
     lat = position.coords.latitude;
     lng = position.coords.longitude;
 
-  /*  console.log(lat, lng);
+    console.log(lat, lng);
     map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: lat, lng: lng },
       zoom: 18
@@ -32,7 +28,7 @@ window.initMap = async function() {
     const marker = new google.maps.Marker({
       position: { lat: lat, lng: lng },
       map: map
-    });*/
+    });
     console.log("Everything is good")
   } catch (err) {
     alert(err.message);
