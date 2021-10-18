8d04d2ed60624fbf0b6cff1366523673734833d5
Glitch (glitch-hello-website) <none>
2021-10-18 16:04:02 +0000

    👫🗽 Checkpoint ./js/map-builder.js:32302306/552

--- js/map-builder.js
+++ js/map-builder.js
@@ -2,10 +2,15 @@ export class builder {
   constructor(google){
     this.google =  google;
     this.maps = {};
+    this.markers = {};
   }
   
   allMap(){
     return this.maps;
+  }
+  
+  mapsCount(){
+    return this.maps.lenght();
   }
   
   singleMap(name){
@@ -13,11 +18,25 @@ export class builder {
   }
   
   newMap(name, mapDiv, options){
-    if(mapDiv){
+    if(!mapDiv){
       throw "Need a div";
     }
     const map = this.google.maps.Map(mapDiv, options);
-    if(this.maps[name]) throw "The name all ready exist!";
+    if(!this.maps[name]) throw "The name all ready exist!";
     this.maps[name] = map; 
+  }
+  
+  newMarker(lat, lng, icon, map_name){
+    if(!lat || !lng) throw "Need a position to mark";
+    
+    if(!map_name){
+      if(this.mapsCount() == 0)
+    }
+    
+    return new this.google.maps.Marker({
+      position: { lat: lat, lng: lng},
+      
+    })
+    
   }
 }
