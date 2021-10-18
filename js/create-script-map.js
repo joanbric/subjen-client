3db32fc282dd99317a430a5a18537543baddac28
Glitch (glitch-hello-website) <none>
2021-10-18 16:15:12 +0000

    🍭🌜 Checkpoint ./js/map-builder.js:32302306/1232 ./js/create-script-map.js:32302306/85

--- js/create-script-map.js
+++ js/create-script-map.js
@@ -1,5 +1,7 @@
 import constants from "./constants.js";
 
-export const scrypt = document.createElement("script");
+const scrypt = document.createElement("script");
 scrypt.src = `https://maps.googleapis.com/maps/api/js?key=${constants.API_KEY}&callback=initMap`; //&v=weekly&channel=2
 scrypt.async = true;
+
+export default scrypt;
