d0521cfe0fe2e2267680f3b7a3c2055c4dd2d794
Glitch (glitch-hello-website) <none>
2021-10-20 18:28:25 +0000

    🐮👐 Checkpoint ./script.js:32302306/712

--- script.js
+++ script.js
@@ -2,25 +2,18 @@ import scrypt from "./js/script-apimap.js"
 import buildMap from "./js/build-map.js";
 import MarkerManager from "./js/MarkerManager.js"
 
-function getCurrentPosition() {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-}
 
 
 window.initMap = async function() {
   let lat, lng;
   try {
-    const position = await getCurrentPosition();
-    lat = position.coords.latitude;
-    lng = position.coords.longitude;
+  
 
     const map = await buildMap();
     const markerManager = new MarkerManager(map);
     const me = markerManager.me;
 
-    const track = [{ lat: lat, lng: lng }];
+    const track = [me.getPosition()];
     let counter = 0;
 
     const flightPath = new google.maps.Polyline({
@@ -33,13 +26,11 @@ window.initMap = async function() {
 
     let watcherID = navigator.geolocation.watchPosition(
       position => {
-        const { latitude, longitude } = position.coords;
-        me.setPosition({
-          lat: latitude,
-          lng: longitude
-        });
-
-        track.push({ lat: latitude, lng: longitude });
+        const currentPosition = { lat: position.latitude, lng: position.longitude };
+        
+        me.setPosition(currentPosition);
+        //map.setCenter(currentPosition);
+        track.push(currentPosition);
         flightPath.setPath(track);
         counter++;
       },
