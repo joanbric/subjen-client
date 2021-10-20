6460d72c147c81e869d5ac9f0bdca07c9ab4a3c4
Glitch (glitch-hello-website) <none>
2021-10-20 20:45:21 +0000

    👦🗼 Checkpoint ./js/Tracker.js:32302306/713

--- js/Tracker.js
+++ js/Tracker.js
@@ -1,18 +1,37 @@
 class Tracker{
-    const track = [];
+  
   constructor(){
-
+    this.tracks = {};
     this.paths = [new google.maps.Polyline({
       //path: track,
       geodesic: true,
       strokeColor: "#FF0000",
       strokeOpacity: 0.8,
-      strokeWeight: 4
+      strokeWeight: 5
     })];
     
   }
   
   trackMe(){
-    
+     const watcherID_me = navigator.geolocation.watchPosition(
+      position => {
+        const currentPosition = { "lat": position.coords.latitude, "lng": position.coords.longitude };
+        
+        me.setPosition(currentPosition);
+        map.setCenter(currentPosition);
+        track.push(currentPosition);
+        flightPath.setPath(track);
+      },
+      null,
+      { enableHighAccuracy: true }
+    );
+
+    flightPath.setMap(map);
   }
+  
+  untrackMe(watcherID){
+    navigator.geolocation.clearWatch(watcherID);
+  }
+  
+  static
 }
